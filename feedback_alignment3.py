3ced27593aecf57b75698bfbef521dfa76e4080a
kohei <tripdancer0916@gmail.com>
2018-06-20 17:26:20 +0900

    add file

--- feedback_alignment3.py
+++ feedback_alignment3.py
@@ -138,7 +138,7 @@ train_size = x_train.shape[0]
 batch_size = 100
 iter_per_epoch = 100
 print("epoch", "\t", "train_acc", "\t", "test_acc", "\t","train_loss","\t", "test_loss")
-for i in range(10000):
+for i in range(1000000):
     batch_mask = cp.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
