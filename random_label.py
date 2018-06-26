b0abdba26c58c6804c55c4f55982e4a3f94d16e3
kohei <tripdancer0916@gmail.com>
2018-06-26 18:08:34 +0900

    minor change!

--- random_label.py
+++ random_label.py
@@ -135,12 +135,12 @@ train_size = x_train.shape[0]
 batch_size = 100
 iter_per_epoch = 100
 print("epoch", "\t", "train_acc", "\t", "test_acc", "train_loss", "test_loss")
-for i in range(500000):
+for i in range(250000):
     batch_mask = cp.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    # mlp.gradient(x_batch, t_batch)
-    mlp.feedback_alignment(x_batch,t_batch)
+    mlp.gradient(x_batch, t_batch)
+    # mlp.feedback_alignment(x_batch,t_batch)
 
     if i % iter_per_epoch == 0:
         train_acc = mlp.accuracy(x_train, t_train)
@@ -163,7 +163,7 @@ train_size = x_train.shape[0]
 batch_size = 100
 iter_per_epoch = 100
 print("epoch", "\t", "train_acc", "\t", "test_acc", "train_loss", "test_loss")
-for i in range(500000):
+for i in range(250000):
     batch_mask = cp.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
