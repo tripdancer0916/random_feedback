265327e44f6db3f196af2d993719534d77ad92dd
tripdancer0916@gmail.com <ritalin202>
2018-07-04 13:59:30 +0900

    change epochs

--- DFA_relu.py
+++ DFA_relu.py
@@ -175,7 +175,7 @@ test_acc_list_FA = []
 train_size = x_train.shape[0]
 batch_size = 100
 iter_per_epoch = 100
-for i in range(5000):
+for i in range(20000):
     batch_mask = cp.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
