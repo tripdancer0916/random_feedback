f0ef381078a7f1bf8725481f47d0bef2a10b52c4
tripdancer0916@gmail.com <ritalin202>
2018-07-04 14:44:48 +0900

    ones matrix

--- DFA_relu.py
+++ DFA_relu.py
@@ -69,7 +69,8 @@ class MLP:
         self.W_f1 = weight_init_std * cp.random.randn(784, hidden_unit)
         self.W_f2 = weight_init_std * cp.random.randn(hidden_unit, hidden_unit)
         self.W_f3 = weight_init_std * cp.random.randn(hidden_unit, 10)
-        self.B3 = cp.random.randn(10,hidden_unit)
+        """
+        self.B3 = cp.random.randn(10, hidden_unit)
         self.B3[self.B3 > 0] = 1
         self.B3[self.B3 < 0] = -1
         self.B3 = weight_init_std * self.B3
@@ -77,6 +78,9 @@ class MLP:
         self.B2[self.B2 > 0] = 1
         self.B2[self.B2 < 0] = -1
         self.B2 = weight_init_std * self.B2
+        """
+        self.B3 = weight_init_std * cp.ones([10, hidden_unit])
+        self.B2 = weight_init_std * cp.ones([10, hidden_unit])
 
     def predict(self, x):
         h1 = cp.dot(x, self.W_f1)
@@ -227,4 +231,4 @@ plt.plot(test_acc_list_FA[20:], label="DFA test acc", color="orange")
 plt.title("DFA for MNIST relu start from 20")
 plt.legend()
 
-plt.savefig("./result/0704/DFA_randomonesmatrix_for_mnist_20start.png")
+plt.savefig("./result/0704/DFA_onesmatrix_for_mnist_20start.png")
