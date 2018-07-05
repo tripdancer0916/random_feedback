99ef5aeb38ae23ba2cdaaff8894569c562ce8c0e
tripdancer0916@gmail.com <ritalin202>
2018-07-05 19:00:17 +0900

    experiment

--- DFA_experiment.py
+++ DFA_experiment.py
@@ -71,13 +71,13 @@ class MLP:
         self.W_f2 = weight_init_std * cp.random.randn(hidden_unit, hidden_unit)
         self.W_f3 = weight_init_std * cp.random.randn(hidden_unit, hidden_unit)
         self.W_f4 = weight_init_std * cp.random.randn(hidden_unit, 10)
-        """
+
         self.B3 = cp.random.randn(10, hidden_unit)
         self.B3[self.B3 > 0] = 1
         self.B3[self.B3 < 0] = -1
         self.B3 = weight_init_std * self.B3
-        """
-        self.B3 = weight_init_std * cp.ones([10, hidden_unit])
+
+        # self.B3 = weight_init_std * cp.ones([10, hidden_unit])
         # for i in range(10):
         #     if cp.random.rand() > 0.5:
         #         self.B3[i] *= -1
