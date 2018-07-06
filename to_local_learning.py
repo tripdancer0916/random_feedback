f249892d5d223d9970beda57cb0120d7f9a8bc2e
tripdancer0916@gmail.com <ritalin202>
2018-07-06 15:50:03 +0900

    experiment 5

--- to_local_learning.py
+++ to_local_learning.py
@@ -77,26 +77,27 @@ class MLP:
         self.B3[self.B3 < 0] = -1
         self.B3 = weight_init_std * self.B3
         """
-        tmp = [-1, 1]
-        d = np.random.choice(tmp, 10)
+        # tmp = [-1, 1]
+        # d = np.random.choice(tmp, 10)
+        d = cp.random.rand(10) * 2 -1
         # d *= weight_init_std
         self.B3 = []
         for i in range(1000):
-            magnification = np.random.rand() * 2 - 1
+            magnification = cp.random.rand() * 2 - 1
             self.B3.append(d*magnification)
         self.B3 = weight_init_std * cp.array(self.B3)
         self.B3 = self.B3.T
 
         self.B2 = []
         for i in range(1000):
-            magnification = np.random.rand() * 2 - 1
+            magnification = cp.random.rand() * 2 - 1
             self.B2.append(d * magnification)
         self.B2 = weight_init_std * cp.array(self.B2)
         self.B2 = self.B2.T
 
         self.B1 = []
         for i in range(1000):
-            magnification = np.random.rand() * 2 - 1
+            magnification = cp.random.rand() * 2 - 1
             self.B1.append(d * magnification)
         self.B1 = weight_init_std * cp.array(self.B1)
         self.B1 = self.B1.T
@@ -283,4 +284,4 @@ plt.legend()
 
 os.makedirs('./result/0706/', exist_ok=True)
 
-plt.savefig("./result/0706/to_local_learning4.png")
+plt.savefig("./result/0706/to_local_learning5.png")
