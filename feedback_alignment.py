19a5eef9907d298651b600fd869881d3104eddca
tripdancer0916@gmail.com <ritalin202>
2018-07-07 16:47:15 +0900

    feedback alignment

--- feedback_alignment.py
+++ feedback_alignment.py
@@ -70,7 +70,7 @@ class MLP:
         self.W_f1 = weight_init_std * cp.random.randn(784, hidden_unit)
         self.W_f2 = weight_init_std * cp.random.randn(hidden_unit, hidden_unit)
         self.W_f3 = weight_init_std * cp.random.randn(hidden_unit, 10)
-        self.B1 = weight_init_std * cp.random.randn(10, hidden_unit)
+        self.B3 = weight_init_std * cp.random.randn(10, hidden_unit)
         self.B2 = weight_init_std * cp.random.randn(hidden_unit, hidden_unit)
 
     def predict(self, x):
