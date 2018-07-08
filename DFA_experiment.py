1267aaa5c6cd0cab66afde34eb2b4cc8e7399a6d
tripdancer0916@gmail.com <ritalin202>
2018-07-08 18:56:41 +0900

    dfa experiment!

--- DFA_experiment.py
+++ DFA_experiment.py
@@ -215,7 +215,7 @@ print("only last layer")
 train_size = x_train.shape[0]
 batch_size = 100
 iter_per_epoch = 100
-for i in range(50000):
+for i in range(100000):
     batch_mask = cp.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
@@ -244,7 +244,7 @@ print("dfa")
 train_size = x_train.shape[0]
 batch_size = 100
 iter_per_epoch = 100
-for i in range(50000):
+for i in range(100000):
     batch_mask = cp.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
