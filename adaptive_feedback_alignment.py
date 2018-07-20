9bd172fceb1958a9c0f938e33f2c7b364ea8e07e
tripdancer0916@gmail.com <ritalin202>
2018-07-20 19:50:00 +0900

    入力の次元数784→3074

--- adaptive_feedback_alignment.py
+++ adaptive_feedback_alignment.py
@@ -80,7 +80,7 @@ hidden_unit = 5000
 
 class MLP:
     def __init__(self, weight_init_std=0.01):
-        self.W_f1 = weight_init_std * cp.random.randn(784, hidden_unit)
+        self.W_f1 = weight_init_std * cp.random.randn(3072, hidden_unit)
         self.W_f2 = weight_init_std * cp.random.randn(hidden_unit, 10)
         self.B1 = weight_init_std * cp.random.randn(10, hidden_unit)
         # self.B2 = weight_init_std * cp.random.randn(hidden_unit, 784)
