e80ea97aceddc55a5ca303fe8242f17e5f75dbac
tripdancer0916@gmail.com <ritalin202>
2018-08-02 15:22:06 +0900

    change learning rate!

--- FA_experiment.py
+++ FA_experiment.py
@@ -152,13 +152,13 @@ class MLP:
         delta_Wf1 = cp.dot(x.T, relu_grad(h1) * delta1)
 
         alpha1 = 0.02
-        # alpha2 = 0.1
-        # alpha3 = 0.05
-        # alpha4 = 0.03
+        alpha2 = 0.05
+        alpha3 = 0.05
+        alpha4 = 0.1
         self.W_f1 -= alpha1 * delta_Wf1
-        self.W_f2 -= alpha1 * delta_Wf2
-        self.W_f3 -= alpha1 * delta_Wf3
-        self.W_f4 -= alpha1 * delta_Wf4
+        self.W_f2 -= alpha2 * delta_Wf2
+        self.W_f3 -= alpha3 * delta_Wf3
+        self.W_f4 -= alpha4 * delta_Wf4
 
 
 mlp = MLP()
