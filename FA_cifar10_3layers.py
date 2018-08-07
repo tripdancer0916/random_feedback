8463ef3834e472d8ebea4f8a3a955831bcce3489
kohei <tripdancer0916@gmail.com>
2018-08-07 18:30:15 +0900

    change weight init std

--- FA_cifar10_3layers.py
+++ FA_cifar10_3layers.py
@@ -88,7 +88,7 @@ hidden_unit3 = 1000
 
 
 class MLP:
-    def __init__(self, weight_init_std=0.015):
+    def __init__(self, weight_init_std=0.02):
         # self.W_f1 = weight_init_std * cp.random.randn(3072, hidden_unit1)
         # self.W_f2 = weight_init_std * cp.random.randn(hidden_unit1, hidden_unit2)
         # self.W_f3 = weight_init_std * cp.random.randn(hidden_unit2, hidden_unit3)
