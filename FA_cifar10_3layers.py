fedad8816b909dd70b6aee60bde68a81a3957c07
tripdancer0916@gmail.com <ritalin202>
2018-08-15 18:58:57 +0900

    change initial configure

--- FA_cifar10_3layers.py
+++ FA_cifar10_3layers.py
@@ -89,16 +89,16 @@ hidden_unit3 = 1000
 
 class MLP:
     def __init__(self, weight_init_std=0.03):
-        self.W_f1 = weight_init_std * cp.random.randn(3072, hidden_unit1)
-        self.W_f2 = weight_init_std * cp.random.randn(hidden_unit1, hidden_unit2)
-        self.W_f3 = weight_init_std * cp.random.randn(hidden_unit2, hidden_unit3)
-        self.W_f4 = weight_init_std * cp.random.randn(hidden_unit3, 10)
+        # self.W_f1 = weight_init_std * cp.random.randn(3072, hidden_unit1)
+        # self.W_f2 = weight_init_std * cp.random.randn(hidden_unit1, hidden_unit2)
+        # self.W_f3 = weight_init_std * cp.random.randn(hidden_unit2, hidden_unit3)
+        # self.W_f4 = weight_init_std * cp.random.randn(hidden_unit3, 10)
         # self.W_f5 = weight_init_std * cp.random.randn(hidden_unit4, 10)
 
-        # self.W_f1 = weight_init_std * cp.zeros([3072, hidden_unit1])
-        # self.W_f2 = 0.032 * cp.zeros([hidden_unit1, hidden_unit2])
-        # self.W_f3 = weight_init_std * cp.zeros([hidden_unit2, hidden_unit3])
-        # self.W_f4 = weight_init_std * cp.zeros([hidden_unit3, 10])
+        self.W_f1 = weight_init_std * cp.zeros([3072, hidden_unit1])
+        self.W_f2 = weight_init_std * cp.zeros([hidden_unit1, hidden_unit2])
+        self.W_f3 = weight_init_std * cp.zeros([hidden_unit2, hidden_unit3])
+        self.W_f4 = weight_init_std * cp.zeros([hidden_unit3, 10])
         # self.W_f5 = weight_init_std * cp.zeros([hidden_unit4, 10])
 
         self.b1 = weight_init_std * cp.zeros(hidden_unit1)
