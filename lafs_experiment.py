d772a7c3a4199949891ca22c96f3feb849189fb2
tripdancer0916@gmail.com <ritalin202>
2018-09-21 21:00:16 +0900

    change batch_size

--- lafs_experiment.py
+++ lafs_experiment.py
@@ -218,7 +218,7 @@ batch_size = 1000
 
 iter_per_epoch = 100
 print("measure accuracy of hidden-layer in the dynamics of DFA learning.")
-batch_mask = cp.random.choice(train_size, 50000)
+batch_mask = cp.random.choice(train_size, 3000)
 x_batch_ = x_train[batch_mask]
 t_batch_ = t_train[batch_mask]
 for i in range(100000):
