548de0109c13a69ebf5a04dffeca65fd45954240
tripdancer0916@gmail.com <ritalin202>
2018-09-24 22:23:21 +0900

    temporarily

--- dynamics_of_DFA.py
+++ dynamics_of_DFA.py
@@ -189,7 +189,7 @@ if __name__ == '__main__':
     t_batch = t_train[batch_mask_]
     mlp.direct_feedback_alignment(x_batch, t_batch, batch_size)
     hidden_train_acc = [[float(mlp.hidden_acc(x_train, j, t_train))] for j in range(4)]
-    train_acc_list = [mlp.accuracy(x_train, t_train)]
+    train_acc_list.append(mlp.accuracy(x_train, t_train))
     for i in range(1000):
         batch_mask_ = cp.random.choice(train_size, batch_size, replace=False)
         x_batch = x_train[batch_mask_]
