97b757a3e5fd78dd3773f89938ada610134120cf
kohei <tripdancer0916@gmail.com>
2018-10-22 18:12:59 +0900

    one layer back propagation

--- fashion_mnist/lafs_one_layer.py
+++ fashion_mnist/lafs_one_layer.py
@@ -79,8 +79,8 @@ class LAFS:
     def __init__(self, weight_init_std=0.032, hidden_unit=800):
         self.h = [0, 0]
 
-        self.W_f1 = cp.zeros([784, hidden_unit])
-        self.W_f2 = cp.zeros([hidden_unit, 10])
+        self.W_f1 = weight_init_std * cp.random.randn(784, hidden_unit)
+        self.W_f2 = weight_init_std * cp.random.randn(hidden_unit, 10)
 
         # self.dB = weight_init_std * cp.random.randn(10, hidden_unit)
 
